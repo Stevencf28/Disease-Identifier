67b99bc6ffba9fd9ecf3eab24517d33ad2fa5a78
Dean Pinlac <dean.pinlac@gmail.com>
2022-04-22 11:51:50 -0400

    Add Logout

--- client/src/components/extras/Header.js
+++ client/src/components/extras/Header.js
@@ -1,6 +1,15 @@
 import { NavLink } from 'react-router-dom';
-
+import { useContext } from 'react';
+import { Button } from '@mui/material';
+import { UserContext } from '../../shared/UserContext';
+import { useNavigate } from 'react-router-dom';
 const Header = (props) => {
+  const { user, logout } = useContext(UserContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  }
   return (
     <div className='navHeader'>
       <NavLink className='navHeaderLink' to='/'>
@@ -9,6 +18,15 @@ const Header = (props) => {
           >COMP-308 Emerging Technologies - Group1
         </span>
       </NavLink>
+      {
+        user && user.auth &&
+        <Button style={{ background: 'white'}}
+          type="button"
+          variant="outlined"
+          onClick={(e) => handleLogout()}
+        > Logout </Button>
+      }
+      
     </div>
   );
 };
