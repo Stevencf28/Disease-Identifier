44cc9c5b64aa7d5722dd154a4c5302f4de44e3e6
Dean Pinlac <dean.pinlac@gmail.com>
2022-04-22 02:20:11 -0400

    Revert "updatedPull"

--- client/src/components/Motivation.js
+++ client/src/components/Motivation.js
@@ -1,23 +1,6 @@
 import React from 'react';
-import {gql, useQuery, useMutation} from "@apollo/client";
 import YoutubeMotivation from './YoutubeMotivation';
 
-const GET_RANDOM_MOTIVATION = gql`
-{
-    randomMotivation{
-        _id
-        tip
-    }
-}
-`
-
-const DELETE_MOTIVATION = gql`
-    mutation DeleteMotivation($_id: String!){
-        deleteMotivation(_id: $_id){
-            _id
-        }
-    }
-`
 
 function Motivation(props)
 {
